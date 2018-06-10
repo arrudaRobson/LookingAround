d936c9055033c5d547fa4ed009c88b8ca00f7aa6
Robson Arruda <robson.arruda@uni9.edu.br>
2018-06-10 02:51:24 -0300

    add new functions and conection with SQLite

--- app/src/main/java/com/laapp/lookingaround/FormularioActivity.java
+++ app/src/main/java/com/laapp/lookingaround/FormularioActivity.java
@@ -1,26 +1,69 @@
 package com.laapp.lookingaround;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.laapp.lookingaround.dao.BancoDAO;
+import com.laapp.lookingaround.model.Banco;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        helper = new FormularioHelper(this);
+
+        /*Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this, "Obrigado por ajudar outros usuários!", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
+    }
+
+    public void abrirLista(View view) {
+        Intent cadastro = new Intent(this, ListaBancosActivity.class);
+        startActivity(cadastro);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_formulario_mapa:
+                Banco banco = helper.pegaBanco();
+                BancoDAO dao = new BancoDAO(this);
+                dao.insere(banco);
+                dao.close();
+
+                Toast.makeText(FormularioActivity.this,"Obrigado por ajudar outros usuários!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/laapp/lookingaround/ListaBancosActivity.java
+++ app/src/main/java/com/laapp/lookingaround/ListaBancosActivity.java
@@ -2,8 +2,15 @@ package com.laapp.lookingaround;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.laapp.lookingaround.dao.BancoDAO;
+import com.laapp.lookingaround.model.Banco;
+
+import java.util.List;
 
 
 public class ListaBancosActivity extends AppCompatActivity {
@@ -13,9 +20,18 @@ public class ListaBancosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_bancos);
 
-        String[] bancos = {"Santander","Itau", "Bradesco"};
+        BancoDAO dao = new BancoDAO(this);
+        List<Banco> bancos = dao.buscaBancos();
+        dao.close();
+
         ListView listaBancos = findViewById(R.id.lista_bancos);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bancos);
+        ArrayAdapter<Banco> adapter = new ArrayAdapter<Banco>(this, android.R.layout.simple_list_item_1, bancos);
         listaBancos.setAdapter(adapter);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lista_bancos, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
